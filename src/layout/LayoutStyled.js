98b448fd9d98c2a1863b2d626a4fb11133ee72de
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-07-17 16:18:33 +0200

    Fix app layout

--- src/layout/LayoutStyled.js
+++ src/layout/LayoutStyled.js
@@ -7,3 +7,23 @@ export const Container = styled.div`
   margin: auto;
   margin-bottom: 5rem;
 `
+
+export const AddTweetMobileBtn = styled.button`
+  background-color: ${({ theme }) => theme.primary500};
+  border-radius: ${({ theme }) => theme.utils.bigRadius};
+  color: ${({ theme }) => theme.white};
+  position: fixed;
+  bottom: 7rem;
+  right: 5%;
+  width: 6rem;
+  height: 6rem;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  ${({ theme }) => theme.media.sm} {
+    display: none;
+  }
+`
