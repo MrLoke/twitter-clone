0b6745fd4830c8cae5408d0b566345a27bea50b9
Michał Grzesiak <darkjjookkeerr@gmail.com>
2021-07-23 22:22:44 +0200

    Create, style search bar component

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,77 @@
+import { useState } from 'react'
+import { useHistory } from 'react-router-dom'
+import { useSelector } from 'react-redux'
+import Autosuggest from 'react-autosuggest'
+import { Container, UserInitials, Avatar } from './SearchBarStyled'
+import './SearchBar.css'
+
+const SearchBar = () => {
+  const [value, setValue] = useState('')
+  const [suggestions, setSuggestions] = useState([])
+  const users = useSelector((state) => state.app.users)
+  const history = useHistory()
+
+  const getSuggestionValue = (suggestion) => suggestion.name
+
+  const renderSuggestion = (suggestion) => {
+    return (
+      <Container
+        onClick={() => history.push(`/profile/${suggestion.userName}`)}>
+        <Avatar src={suggestion.photoURL} alt={`${suggestion.displayName}`} />
+        <UserInitials>
+          <p>{suggestion.displayName}</p>
+          <p>@{suggestion.userName}</p>
+        </UserInitials>
+      </Container>
+    )
+  }
+
+  const getSuggestions = (value) => {
+    const inputValue = value.trim().toLowerCase()
+    const inputLength = inputValue.length
+
+    return inputLength === 0
+      ? []
+      : users.filter(
+          (user) =>
+            user.displayName.toLowerCase().slice(0, inputLength) ===
+              inputValue ||
+            user.userName.toLowerCase().slice(0, inputLength) === inputValue
+          // search by display name or user name
+        )
+  }
+
+  const handleOnChange = (e, { newValue }) => {
+    setValue(newValue)
+  }
+
+  const onSuggestionsFetchRequested = async ({ value }) => {
+    console.log(value)
+    setSuggestions(getSuggestions(value))
+  }
+
+  const onSuggestionsClearRequested = () => {
+    setSuggestions([])
+  }
+
+  const inputProps = {
+    placeholder: 'Search twitter',
+    value,
+    onChange: handleOnChange,
+  }
+
+  return (
+    <>
+      <Autosuggest
+        suggestions={suggestions}
+        onSuggestionsFetchRequested={onSuggestionsFetchRequested}
+        onSuggestionsClearRequested={onSuggestionsClearRequested}
+        getSuggestionValue={getSuggestionValue}
+        renderSuggestion={renderSuggestion}
+        inputProps={inputProps}
+      />
+    </>
+  )
+}
+
+export default SearchBar
---
+++ src/components/SearchBar/SearchBarStyled.js
@@ -0,0 +1,20 @@
+import styled from 'styled-components/macro'
+
+export const Container = styled.div`
+  display: flex;
+`
+
+export const Avatar = styled.img`
+  width: 5rem;
+  height: 5rem;
+  object-fit: cover;
+  margin-right: 1rem;
+  border-radius: 50%;
+  cursor: pointer;
+`
+
+export const UserInitials = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+`
